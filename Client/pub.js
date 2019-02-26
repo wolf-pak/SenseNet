62568c7cc19e014d45903dd216e532004d1e5bd0
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2019-02-26 14:11:02 +0100

    Generisk kod
    Jag har lagt till funktionalitet för att vara mer generisk. Det funkar att logga arrayen messageArray och vi får ut rätt värden men den vill inte publisha ordentligt, något tokigt med rad kod 61 och därefter.

--- Client/pub.js
+++ Client/pub.js
@@ -10,38 +10,65 @@ setInterval(function(){
     var fs =  require('fs');
     msgnr ++;
     
+    var sensorArray = [];
+    var messageArray = [];
+    
     //Gets system temperature
    var sys_temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
    if (sys_temperature > 570){
-    temperature = ((sys_temperature/1000).toPrecision(3));
+    temperature = {
+        "value" : ((sys_temperature/1000).toPrecision(3)),
+        "type"  : "temperature",
+        "sensorId" : "temperatureOne"
+    }
+    sensorArray.push(temperature); 
+
+    //Påhittad ljussensor som tar värmen
+    var sys_light = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
+    light = {
+       "value" : ((sys_light/1000).toPrecision(3)),
+       "type"  : "light",
+       "sensorId" : "lightOne"     
+    }
+    sensorArray.push(light); 
+
+    //Påhittad ljussensor som tar värmen
+    var sys_light1 = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
+    light1 = {
+       "value" : ((sys_light1/1000).toPrecision(3)),
+       "type"  : "light",
+       "sensorId" : "lightTwo"  
+    }
+    sensorArray.push(light1); 
+     
     var time = new Date();
     var res = time.toString().substring(4, 26);
-    var type = "Light"; //Hardcoded value for light sensor.
-    var type1 = "Vibration"; //Hardcoded value for vibration sensor.
 
-    //Json
-    var message = { 
-        "value"  :  temperature, 
-        "time_stamp"   :  time,
-        "type" : type
-      }
+    
+    for(i = 0; i < sensorArray.length; i++){
+        var message = { 
+            "hostname" : hostname,
+            "value"  :  sensorArray[i].value, 
+            "time_stamp"   :  time,
+            "type" : sensorArray[i].type,
+            "sensorId" : sensorArray[i].sensorId
+        }
+        messageArray.push(message);
+    }
 
-    var message1 = { 
-        "value"  :  temperature, 
-        "time_stamp"   :  time,
-        "type" : type1
-      }
-
-    var topic = hostname + "/" + type;
-    var topic1 = hostname + "/" + type1;
-
+    console.log(messageArray);
     //Publishes message
         client.on('connect', function () {
-        client.publish(topic, JSON.stringify(message))
-        client.publish(topic1, JSON.stringify(message1)) })
-        console.log("--------------------------------------------------------");
-        console.log("Sucessfully sent: " + message.value + "     |     " + res);
-        console.log("Nr: " + msgnr + " since start");
+            
+        for(i = 0; i < messageArray; i++){
+            var message = messageArray[i];
+            var topic = hostname //+ "/" + message.type + "/" + message.sensorId;
 
+           
+            client.publish(topic, JSON.stringify(message))
+            console.log("--------------------------------------------------------");
+            console.log("Sucessfully sent: " + message.value + "     |     " + res);
+            console.log("Nr: " + msgnr + " since start");
+        } })
     }	
-    },2000);
+},2000);
