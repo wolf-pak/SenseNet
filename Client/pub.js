70d76a51ef9708cce7aa06106b38f48029496771
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2019-02-27 11:26:56 +0100

    Förbättring

--- Client/pub.js
+++ Client/pub.js
@@ -8,66 +8,48 @@ setInterval(function(){
     var mqtt = require('mqtt')
     var client  = mqtt.connect('mqtt:192.168.4.1')
     var fs =  require('fs');
+    var time = new Date();
+    var res = time.toString().substring(4, 26);
     msgnr ++;
     
-    var sensorArray = [];
     var messageArray = [];
+
+    function Sensor(value, type, sensorId) {
+        this.hostname = hostname;
+        this.time = time;
+        this.value = value;
+        this.type = type;
+        this.sensorId = sensorId;
+    }
     
     //Gets system temperature
    var sys_temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
    if (sys_temperature > 570){
-    temperature = {
-        "value" : ((sys_temperature/1000).toPrecision(3)),
-        "type"  : "temperature",
-        "sensorId" : "temperatureOne"
-    }
-    sensorArray.push(temperature); 
+    var sys_temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
+    var temperature = new Sensor(((sys_temperature/1000).toPrecision(3)), "light", "lightOne")
+    messageArray.push(temperature); 
 
     //Påhittad ljussensor som tar värmen
     var sys_light = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
-    light = {
-       "value" : ((sys_light/1000).toPrecision(3)),
-       "type"  : "light",
-       "sensorId" : "lightOne"     
-    }
-    sensorArray.push(light); 
+    var light = new Sensor(((sys_light/1000).toPrecision(3)), "light", "lightOne")
+    messageArray.push(light); 
 
     //Påhittad ljussensor som tar värmen
     var sys_light1 = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
-    light1 = {
-       "value" : ((sys_light1/1000).toPrecision(3)),
-       "type"  : "light",
-       "sensorId" : "lightTwo"  
-    }
-    sensorArray.push(light1); 
-     
-    var time = new Date();
-    var res = time.toString().substring(4, 26);
+    var light1 = new Sensor(((sys_light1/1000).toPrecision(3)), "light", "lightTwo")
+    messageArray.push(light1); 
 
-    
-    for(i = 0; i < sensorArray.length; i++){
-        var message = { 
-            "hostname" : hostname,
-            "value"  :  sensorArray[i].value, 
-            "time_stamp"   :  time,
-            "type" : sensorArray[i].type,
-            "sensorId" : sensorArray[i].sensorId
-        }
-        messageArray.push(message);
-    }
 
-    console.log(messageArray);
+    //console.log(messageArray);
     //Publishes message
         client.on('connect', function () {
             
-        for(i = 0; i < messageArray; i++){
+        for(i = 0; i < messageArray.length; i++){
             var message = messageArray[i];
-            var topic = hostname //+ "/" + message.type + "/" + message.sensorId;
-
-           
+            var topic = hostname + "/" + message.type + "/" + message.sensorId;
             client.publish(topic, JSON.stringify(message))
             console.log("--------------------------------------------------------");
-            console.log("Sucessfully sent: " + message.value + "     |     " + res);
+            console.log("Sucessfully sent: " + JSON.stringify(message.value) + "     |     " + res);
             console.log("Nr: " + msgnr + " since start");
         } })
     }	
