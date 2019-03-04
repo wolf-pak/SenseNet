73004f75c5595f757845e1866f37c20075e43395
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2019-03-04 16:29:30 +0100

    Förbättring
    Fixat koden för pub

--- Client/pub.js
+++ Client/pub.js
@@ -1,84 +1,75 @@
 'use strict'
+console.log("Successfuly launched script.");
+var os = require("os");
+var hostname = os.hostname();
+var mqtt = require('mqtt')
+var fs = require('fs');
+//var path = require('path')
+//var clientoptions = fs.readFileSync(path.join("/home/nodejs","/clientoptions.js"))
+var clientoptions = require('/home/nodejs/clientoptions');
 
-console.log("Successfuly launched script."); 
-var msgnr = 0;     
-setInterval(function(){
-    //gets hostname for topic
-    var os = require("os");
-    var hostname = os.hostname();
-    //Initializes mqtt
-    var mqtt = require('mqtt')
-    var fs =  require('fs');
-    var path = require('path')
-    var KEY = fs.readFileSync(path.join("/home/nodejs", "/client.key"))
-    var CERT = fs.readFileSync(path.join("/home/nodejs","/client.crt"))
-    var TRUSTED_CA_LIST = fs.readFileSync(path.join("/home/nodejs", "/ca.srl"))
-
-    //var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+var options = clientoptions.fetchoptions();
+var msgnr = 0;
+var messageArray = [];
 
 
-    var PORT = 8883 // 8883
-    var HOST = '192.168.4.1' //Bill
-  
-    var options = {
-        //clientId : clientId,
-        username : 'Jan',
-        password : 'raspberry',
-        port: PORT,
-        host: HOST,
-        key: KEY,
-        cert: CERT,
-        rejectUnauthorized: false,
-        // The CA list will be used to determine if server is authorized
-        ca: TRUSTED_CA_LIST,
-        protocol: 'mqtts'
-    }
-    
-    var client  = mqtt.connect(options)  //'mqtt:192.168.4.1'  //client.tls_set("/etc/mosquitto/ca_certificates/ca.crt")
+function SensorState(value, type, sensorId, time) {
+    this.hostname = hostname;
+    this.time = time;
+    this.value = value;
+    this.type = type;
+    this.sensorId = sensorId;
+}
 
-    msgnr ++;
-    var time = new Date();
-    var res = time.toString().substring(4, 26);
-    var messageArray = [];
+var temperature = new SensorState(0, "temperature", "temperatureOne", new Date());
+messageArray.push(temperature);
 
-    function Sensor(value, type, sensorId) {
-        this.hostname = hostname;
-        this.time = time;
-        this.value = value;
-        this.type = type;
-        this.sensorId = sensorId;
-    }
-    
-    //Gets system temperature
-   var sys_temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
-   if (sys_temperature > 570){
-    var sys_temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
-    var temperature = new Sensor(((sys_temperature/1000).toPrecision(3)), "light", "lightOne")
-    messageArray.push(temperature); 
+var light1 = new SensorState(0, "light", "lightOne", new Date());
+messageArray.push(light1);
 
-    //Påhittad ljussensor som tar värmen
-    var sys_light = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
-    var light = new Sensor(((sys_light/1000).toPrecision(3)), "light", "lightOne")
-    messageArray.push(light); 
+var light2 = new SensorState(0, "light", "lightTwo", new Date());
+messageArray.push(light2);
 
-    //Påhittad ljussensor som tar värmen
-    var sys_light1 = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");    
-    var light1 = new Sensor(((sys_light1/1000).toPrecision(3)), "light", "lightTwo")
-    messageArray.push(light1); 
+var client = mqtt.connect(options);  //client.tls_set("/etc/mosquitto/ca_certificates/ca.crt")
 
+// if (sys_temperature > 570){   //}	
 
-    console.log("Utanför Connectmetoden.");
-    //Publishes message
-        client.on('connect', function (err) {
-            console.log("Innanför Connectmetoden.");
-            
-        for(var i = 0; i < messageArray.length; i++){
+console.log("Utanför Connectmetoden.");
+//Publishes message
+client.on('connect', function (err) {
+    console.log("Innanför Connectmetoden.");
+
+    setInterval(function () {
+
+        msgnr++;
+
+        for (var i = 0; i < messageArray.length; i++) {
+            switch (messageArray[i].type) {
+                case "temperature":
+                    var sys_temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
+                    var tempTemperature = ((sys_temperature / 1000).toPrecision(3));
+                    messageArray[i].value = tempTemperature;
+                    messageArray[i].date = new Date();
+                    break;
+                case "light":
+                    var sys_light = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
+                    var tempLight = ((sys_light / 1000).toPrecision(3));
+                    messageArray[i].value = tempLight;
+                    messageArray[i].date = new Date();
+                    break;
+                default:
+                    break;
+            }
+
             var message = messageArray[i];
             var topic = hostname + "/" + message.type + "/" + message.sensorId;
             client.publish(topic, JSON.stringify(message))
+
             console.log("--------------------------------------------------------");
-            console.log("Sucessfully sent: " + JSON.stringify(message.value) + "     |     " + res);
+            console.log("Sucessfully sent: " + JSON.stringify(message.value) + "     |     " + new Date().toString().substring(4, 26));
             console.log("Nr: " + msgnr + " since start");
-        } })
-    }	
-},2000);
+        }
+
+    }, 2000);
+})
+
