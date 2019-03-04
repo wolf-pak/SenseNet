b230d8125eead79e380603c9782fd6f45840d95e
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2019-03-04 16:39:14 +0100

    Förbättring
    Lagt in så att även sub hämtar options från den separata filen

--- Client/sub.js
+++ Client/sub.js
@@ -1,53 +1,31 @@
-console.log("Successfuly launched script.");      
+console.log("Successfuly launched script.");
 
 //Init
-    //Initializes mqtt
-    var mqtt = require('mqtt')
-    var fs =  require('fs');
-    var path = require('path')
-    var KEY = fs.readFileSync(path.join("/home/nodejs", "/client.key"))
-    var CERT = fs.readFileSync(path.join("/home/nodejs","/client.crt"))
-    var TRUSTED_CA_LIST = fs.readFileSync(path.join("/home/nodejs", "/ca.srl"))
-
-    //var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
-
+//Initializes mqtt
+var mqtt = require('mqtt')
+var clientoptions = require('/home/nodejs/clientoptions');
+var options = clientoptions.fetchoptions();
 
 var dblite = require('dblite'),
   db = dblite('/home/broker/brokerDB.db');
 
-  var PORT = 8883 // 8883
-  var HOST = '192.168.4.1' //Bill
-  
-   var options = {
-      //clientId : clientId,
-      username : 'Jan',
-      password : 'raspberry',
-      port: PORT,
-      host: HOST,
-      key: KEY,
-      cert: CERT,
-      rejectUnauthorized: false,
-      // The CA list will be used to determine if server is authorized
-      ca: TRUSTED_CA_LIST,
-      protocol: 'mqtts'
-  }
-    
-  var client  = mqtt.connect(options)
+
+var client = mqtt.connect(options)
 
 //Connect
 client.on('connect', function () {
   client.subscribe('#', function (err) {
     if (!err) {
-     console.log("Connection sucessful: Currently subscribing to #");
+      console.log("Connection sucessful: Currently subscribing to #");
     }
   })
 })
 
 //On Message recieved
 client.on('message', function (topic, message) {
-	var top = topic.toString();
-	var mess = message.toString();
-  console.log('Received message:'+ mess);
+  var top = topic.toString();
+  var mess = message.toString();
+  console.log('Received message:' + mess);
 
   //JSON
   var obj = JSON.parse(mess);
@@ -55,14 +33,14 @@ client.on('message', function (topic, message) {
   var time = obj.time_stamp;
 
   //Enters message into DB
- 	db.query('INSERT INTO sensorData VALUES(null, ?, ?, ?)', [top, value, time]);
- 	console.log('Database: Inserted: ' + value + " from: " + top + ". Recorded at: " + time);
+  db.query('INSERT INTO sensorData VALUES(null, ?, ?, ?)', [top, value, time]);
+  console.log('Database: Inserted: ' + value + " from: " + top + ". Recorded at: " + time);
 })
 
 //Queries the latest DB entry to ensure correct functionality.
-setInterval(function(){
-	console.log('Database printout (latest entry):');
-	db.query('SELECT * FROM sensorData ORDER BY id DESC LIMIT 1;');
+setInterval(function () {
+  console.log('Database printout (latest entry):');
+  db.query('SELECT * FROM sensorData ORDER BY id DESC LIMIT 1;');
 }, 2000);
 /* -----Ghost Code ------
 db.query ('DROP TABLE sensorData');
