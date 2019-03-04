4439e2f9075d6834c974b9e7df0961eabe413555
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2019-03-04 11:53:47 +0100

    Test sub

--- Client/sub.js
+++ Client/sub.js
@@ -1,10 +1,38 @@
 console.log("Successfuly launched script.");      
 
 //Init
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt:192.168.4.1')
+    //Initializes mqtt
+    var mqtt = require('mqtt')
+    var fs =  require('fs');
+    var path = require('path')
+    var KEY = fs.readFileSync(path.join("/home/nodejs", "/client.key"))
+    var CERT = fs.readFileSync(path.join("/home/nodejs","/client.crt"))
+    var TRUSTED_CA_LIST = fs.readFileSync(path.join("/home/nodejs", "/ca.srl"))
+
+    //var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+
+
 var dblite = require('dblite'),
-    db = dblite('brokerDB.db');
+  db = dblite('brokerDB.db');
+
+  var PORT = 8883 // 8883
+  var HOST = '192.168.4.1' //Bill
+  
+   var options = {
+      //clientId : clientId,
+      username : 'Jan',
+      password : 'raspberry',
+      port: PORT,
+      host: HOST,
+      key: KEY,
+      cert: CERT,
+      rejectUnauthorized: false,
+      // The CA list will be used to determine if server is authorized
+      ca: TRUSTED_CA_LIST,
+      protocol: 'mqtts'
+  }
+    
+  var client  = mqtt.connect(options)
 
 //Connect
 client.on('connect', function () {
