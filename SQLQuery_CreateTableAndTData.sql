b2ccc7637519297d4e47cba4f96cf5cd5e9fdb0f
jerterius <33423806+jerterius@users.noreply.github.com>
2019-03-06 11:08:42 +0100

    Supplementary adds on SQL queries

--- SQLQuery_CreateTableAndTData.sql
+++ SQLQuery_CreateTableAndTData.sql
@@ -1,17 +1,19 @@
 CREATE TABLE SensorState(
 	id int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	time datetime NOT NULL,
-	value varchar(255) NULL,
-	node varchar(255) NULL,
-	sensorId varchar(255) NULL
+	value varchar(255) NOT NULL,
+	type varchar(255) NOT NULL,
+	node varchar(255) NOT NULL,
+	sensorId varchar(255) NOT NULL
 )
 
-INSERT INTO SensorState (time, value, node, sensorId) VALUES(
+INSERT INTO SensorState (time, value, type, node, sensorId) VALUES(
 CURRENT_TIMESTAMP,
 '53.6',
+'type',
 'testNode',
 'testSensor'
 )
 
 
-SELECT * FROM SensorState
+DROP TABLE SensorState
