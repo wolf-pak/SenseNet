b06923a7f5532de07e3a947a5b9b67981db1bd6f
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2019-03-07 13:15:13 +0100

    Tillägg och struktur
    Spaltat ut lite mer javascript kod och lagt det på ett bättre ställe samt lagt till en sida för en graf i navbaren. Kan slänga in färdig template där och fylla med data.

---
+++ SenseNetClient/SenseNetCloud/Scripts/Javascript/getData.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    // Proxy created on the fly
+    var data = $.connection.dataHub;
+    // Declare a function on the job hub so the server can invoke it
+
+    data.client.displayData = function () {
+        getData();
+    };
+    // Start the connection
+    $.connection.hub.start();
+    getData();
+});
+
+function getData() {
+    var $tbl = $('#tblInfo');
+    $.ajax({
+        url: $("#Get").val(),
+        type: 'GET',
+        datatype: 'json',
+        success: function (data) {
+            $tbl.empty();
+            $.each(data.listData, function (i, model) {
+                $tbl.prepend
+                    (
+                    '<tr>' +
+                    '<td>' + model.Id + '</td>' +
+                    '<td>' + new Date(model.Time) + '</td>' +
+                    '<td>' + model.Value + '</td>' +
+                    '<td>' + model.Type + '</td>' +
+                    '<td>' + model.Node + '</td>' +
+                    '<td>' + model.SensorId + '</td>' +
+                    '<tr>'
+                    );
+            });
+        }
+    });
+}
