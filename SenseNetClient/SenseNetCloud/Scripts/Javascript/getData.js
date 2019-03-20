fbf41caef4d428dcc92be02e3cb625fde2514dc1
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2019-03-20 16:47:00 +0100

    Tappra försök

--- SenseNetClient/SenseNetCloud/Scripts/Javascript/getData.js
+++ SenseNetClient/SenseNetCloud/Scripts/Javascript/getData.js
@@ -1,27 +1,29 @@
-﻿
-
-function getTableData() {
+﻿function getTableData() {
 
     var $tbl = $('#tblInfo');
+
     $.ajax({
         url: $("#Get").val(),
         type: 'GET',
         datatype: 'json',
         success: function (data) {
-            $tbl.empty();
             $.each(data.listData, function (i, model) {
                 $tbl.prepend
                     (
-                    '<tr>' +
+                    '<tr>' + 
                     '<td>' + model.Id + '</td>' +
                     '<td>' + new Date(model.Time) + '</td>' +
                     '<td>' + model.Value + '</td>' +
                     '<td>' + model.Type + '</td>' +
                     '<td>' + model.Node + '</td>' +
                     '<td>' + model.SensorId + '</td>' +
-                    '<tr>'
+                    '</tr>'
                     );
             });
         }
     });
+
+       
 }
+
+
